4ebcb34affc72ef3d7708f12350b161794de722a
Muhammad Yusuf <muhammadyusuf@MacBook-Air-Muhammad.local>
2024-08-01 21:21:43 +0700

    fixing sidebar menu

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,75 +1,33 @@
-import React, { useState } from 'react';
-import { MailOutlined } from '@ant-design/icons';
+import React from 'react';
+import { AppstoreOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
-
 const items = [
   {
-    key: '1',
-    icon: <MailOutlined />,
-    label: 'Navigation One',
+    key: 'grp',
+    label: 'List',
+    type: 'group',
     children: [
       {
-        key: '11',
-        label: 'Option 1',
-      },
-      {
-        key: '12',
-        label: 'Option 2',
-      },
-      {
         key: '13',
-        label: 'Option 3',
-      },
-      {
-        key: '14',
-        label: 'Option 4',
-      },
+        label: 'Transactions',
+        icon: <AppstoreOutlined />,
+      }
     ],
-  }
+  },
 ];
-const getLevelKeys = (items1) => {
-  const key = {};
-  const func = (items2, level = 1) => {
-    items2.forEach((item) => {
-      if (item.key) {
-        key[item.key] = level;
-      }
-      if (item.children) {
-        func(item.children, level + 1);
-      }
-    });
-  };
-  func(items1);
-  return key;
-};
-const levelKeys = getLevelKeys(items);
-
 const BaseSidebarComponent = () => {
-  const [stateOpenKeys, setStateOpenKeys] = useState(['1', '2']);
-  const onOpenChange = (openKeys) => {
-    const currentOpenKey = openKeys.find((key) => stateOpenKeys.indexOf(key) === -1);
-    if (currentOpenKey !== undefined) {
-      const repeatIndex = openKeys
-        .filter((key) => key !== currentOpenKey)
-        .findIndex((key) => levelKeys[key] === levelKeys[currentOpenKey]);
-      setStateOpenKeys(
-        openKeys
-          .filter((_, index) => index !== repeatIndex)
-          .filter((key) => levelKeys[key] <= levelKeys[currentOpenKey]),
-      );
-    } else {
-      setStateOpenKeys(openKeys);
-    }
+  const onClick = (e) => {
   };
+
   return (
     <Menu
-      mode="inline"
-      openKeys={stateOpenKeys}
-      defaultSelectedKeys={['1']}
-      onOpenChange={onOpenChange}
+      onClick={onClick}
       style={{
-        width: 350,
+        width: 256,
       }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
       items={items}
       className="h-screen"
     />
